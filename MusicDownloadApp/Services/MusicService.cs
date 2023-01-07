f9e9fae4a6a01443e0a408f879ac02cdb4806172
Bayr_kf60 <Bayr_kf60@STHQ012B-09>
2023-01-07 17:32:11 +0400

    Add project files.

---
+++ MusicDownloadApp/Services/MusicService.cs
@@ -0,0 +1,32 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoLibrary;
+
+namespace MusicDownloadApp.Services
+{
+    public class MusicService
+    {
+        public async void SaveMP3(string SaveToFolder, string VideoURL, string MP3Name)
+        {
+            string source = SaveToFolder;
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(VideoURL);
+            string videopath = Path.Combine(source, vid.FullName);
+            File.WriteAllBytes(videopath, vid.GetBytes());
+            var inputFile = new MediaFile { Filename = Path.Combine(source, vid.FullName) };
+            var outputFile = new MediaFile { Filename = Path.Combine(source, $"{MP3Name}.mp3") };
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+                engine.Convert(inputFile, outputFile);
+            }
+            File.Delete(Path.Combine(source, vid.FullName));
+        }
+    }
+}
